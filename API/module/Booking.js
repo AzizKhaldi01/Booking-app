76d7e67305efd12ff568ee9e9e8a52288c20216e
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-08-31 18:19:25 +0100

    Add files via upload

--- API/module/Booking.js
+++ API/module/Booking.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose')
-const {Schema} =  mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-
-const BookingSchema = new Schema ({
-     Place: {type: mongoose.Schema.Types.ObjectId , require:true},
-  User: {type: mongoose.Schema.Types.ObjectId , require:true},
-    checkIn:{type:Date , require:true },
-    days:Number ,
-    checkOut:{type:Date , require:true },
-    name:{type:String , require:true },
-   phone :{type:String , require:true },
-    price:Number,
-
-     
-
-})
- const BookingModel = mongoose.model('Booking',BookingSchema);
- module.exports=BookingModel;
+const BookingSchema = new Schema({
+  Place: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "Place" },
+  User: { type: mongoose.Schema.Types.ObjectId, require: true },
+  checkIn: { type: Date, require: true },
+  days: Number,
+  checkOut: { type: Date, require: true },
+  name: { type: String, require: true },
+  phone: { type: String, require: true },
+  price: Number,
+});
+const BookingModel = mongoose.model("Booking", BookingSchema);
+module.exports = BookingModel;
