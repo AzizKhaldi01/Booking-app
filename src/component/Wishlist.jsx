c849a84372b5a5c40877a468e8f4a4b4722fc4db
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-09-10 18:03:10 +0200

    Add files via upload

--- src/component/Wishlist.jsx
+++ src/component/Wishlist.jsx
@@ -14,15 +14,31 @@ useEffect(()=>{
 console.log(data)
   return (
     <div className='  flex flex-col pt-16 w-full h-full  justify-center px-3  md:px-12'>
-       <div className='  w-full h-full grid md:grid-cols-2 grid-cols-1 gap-3  lg:grid-cols-3 '>
+       <div className='  w-full   grid md:grid-cols-2 grid-cols-1 gap-3  lg:grid-cols-3 '>
       
        { data?.map((item) =>(
-        <div className=' flex flex-row gap-2 w-full    p-2  cursor-pointer h-[240px] '>
-<img className=' h-full w-[60%] rounded-l-3xl object-cover  ' src={` http://localhost:4000/uploads/${item?.Place.photos[0] }`} alt="" />
+        <div className=' flex flex-row gap-2 w-full    p-2  cursor-pointer h-[250px]   '>
+<img className='  h-full  w-[60%] rounded-l-3xl object-cover  ' src={` http://localhost:4000/uploads/${item?.Place.photos[0] }`} alt="" />
 
-<div className=' gap-2 flex flex-col h-full w-[40%] '>
-<img   className=' object-cover  h-full rounded-tr-3xl ' src={` http://localhost:4000/uploads/${item?.Place.photos[1] }`} alt="" />
-<img  className=' object-cover   h-full rounded-br-3xl' src={` http://localhost:4000/uploads/${item?.Place.photos[2] }`} alt="" />
+<div className=' gap-2 flex flex-col  w-[40%] '>
+<div className=' rounded-tr-3xl' style={{backgroundImage: `url(${` http://localhost:4000/uploads/${item?.Place.photos[1] }`})`,
+    backgroundSize: 'cover', // You can adjust this to 'contain' or other values as needed
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    height: '100vh',
+      }}>
+
+</div>
+
+ <div className=' rounded-br-3xl' style={{backgroundImage: `url(${` http://localhost:4000/uploads/${item?.Place.photos[0] }`})`,
+    backgroundSize: 'cover', // You can adjust this to 'contain' or other values as needed
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    height: '100vh',
+      }}>
+
+</div>
+ 
 </div>
         </div>
        ) ) }
