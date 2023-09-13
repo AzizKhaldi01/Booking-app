f9ef77d7002f69638dfb793141e8c4ec6122ef41
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-09-13 22:47:58 +0200

    Add files via upload

---
+++ src/component/MobileNav.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LuggageOutlinedIcon from "@mui/icons-material/LuggageOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { useContext } from "react";
+import { Usercontext } from "../context/pagecontext";
+
+function MobileNav() {
+const {User} = useContext(Usercontext);
+
+  const location = useLocation();
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
+  return (
+    <div className="w-full md:hidden bg-white z-10 flex flex-row items-center justify-around h-20 border-solid border-[1px] fixed bottom-0 right-0">
+     { User ?   <> <Link
+        to={"/"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <HomeOutlinedIcon
+          className={isActive("/") ? "text-main" : `text-gray-400`}
+          fontSize="large"
+        />{" "}
+        Home
+      </Link>
+      <Link
+        to={"/account/Wishlist"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/account/Wishlist") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <FavoriteBorderOutlinedIcon
+          className={
+            isActive("/account/Wishlist") ? "text-main" : `text-gray-400`
+          }
+          fontSize="large"
+        />{" "}
+        Favorite
+      </Link>
+      <Link
+        to={"/account/Trips"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/account/Trips") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <LuggageOutlinedIcon
+          className={isActive("/account/Trips") ? "text-main" : `text-gray-400`}
+          fontSize="large"
+        />{" "}
+        Trips
+      </Link>
+      <Link
+        to={"/account"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/account") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <PersonOutlineOutlinedIcon
+          className={isActive("/account") ? "text-main" : `text-gray-400`}
+          fontSize="large"
+        />{" "}
+        Profile
+      </Link></> 
+      
+      :
+
+
+      <>
+      <Link
+        to={"/"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <SearchOutlinedIcon
+          className={isActive("/") ? "text-main" : `text-gray-400`}
+          fontSize="large"
+        />{" "}
+        Search
+      </Link>
+      <Link
+        to={"/account/Wishlist"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/account/Wishlist") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <FavoriteBorderOutlinedIcon
+          className={
+            isActive("/account/Wishlist") ? "text-main" : `text-gray-400`
+          }
+          fontSize="large"
+        />{" "}
+        Favorite
+      </Link>
+       
+      <Link
+        to={"/login"}
+        className={`flex flex-col items-center justify-center text-xs ${
+          isActive("/login") ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <PersonOutlineOutlinedIcon
+          className={isActive("/login") ? "text-main" : `text-gray-400`}
+          fontSize="large"
+        />{" "}
+        Log in
+      </Link></>
+      }
+    </div>
+  );
+}
+
+export default MobileNav;
