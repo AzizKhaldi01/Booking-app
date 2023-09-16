1735a3d0d54bbad0739af8b924b94bfd0f907594
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-09-16 21:03:14 +0200

    Add files via upload

--- API/module/User.js
+++ API/module/User.js
@@ -3,7 +3,8 @@ const {Schema} =  mongoose
 
 
 const UserSchema = new Schema ({
-    name:String,
+    firstname:String,
+    lastname:String,
     email:{type:String , unique:true},
     password:String,
 
