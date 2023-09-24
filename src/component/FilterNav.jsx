10fffcb2da6df258d7d58501becf9361d29753f3
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-09-24 20:03:59 +0100

    Add files via upload

--- src/component/FilterNav.jsx
+++ src/component/FilterNav.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function FilterNav({img , text ,setUrlFilter}) {
   return (
-    <Link to={ `/${setUrlFilter}` }  className=' flex flex-col gap-1 items-center justify-start   cursor-pointer  opacity-70 hover:opacity-100  text-black   '>
-        <img className='  w-[28px] ' src={img}   />
-        <p className=' text-gray-700 text-xs'>{text} </p>
+    <Link onClick={setUrlFilter(text)} className=' flex flex-col gap-[.4rem] items-center justify-start   cursor-pointer  opacity-70 hover:opacity-100  text-black   '>
+        <img className='  w-[28px]  md:w-[31px] ' src={img}   />
+        <p className=' text-gray-700 text-[14px] '>{text} </p>
     </Link>
   )
 }
