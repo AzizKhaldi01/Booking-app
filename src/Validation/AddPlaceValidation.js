6a544902bd5c1c6d17c8858acb088515aeea1f4d
AzizKhaldi <119711102+AzizKhaldi01@users.noreply.github.com>
2023-10-12 20:26:51 +0100

    Add files via upload

--- src/Validation/AddPlaceValidation.js
+++ src/Validation/AddPlaceValidation.js
@@ -2,15 +2,12 @@ import * as yup from 'yup'
 
 
 export const PlaceSchema = yup.object().shape({
-    title: yup.string().required(),
+    title: yup.string().max(60).required(),
     address: yup.string().required(),
-    description: yup.string().max(231).required(),
-    extraInfo: yup.string().required(),
+    description: yup.string().max(600).required(),
+    extraInfo: yup.string().max(3000).required(),
     checkIn: yup.number('Check in time must be a number').positive().integer('Check in time must be a number').required(),
     checkOut: yup.number().positive().integer('Check out time must be a number').required(),
-    maxGuests: yup.number('Max Guests must be a number')
-    .positive('Max Guests must be a positive number')
-    .integer('Max Guests must be an integer')
-    .required('Max Guests is required'),
+   
     price :yup.number().positive().integer().required(),
 })
